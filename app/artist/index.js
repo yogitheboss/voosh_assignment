5ea346a93d45fd8a4be3f2a18e9b54c52e970792
Yograj Rajput <91418287+yogitheboss@users.noreply.github.com>
2024-12-10 18:09:54 +0530

    fix route

--- app/artist/index.js
+++ app/artist/index.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.get("/", authorize(["user"]), getArtists);
 router.get("/:id", authorize(["user"]), getArtist);
+router.put("/:id", authorize(["editor"]), updateArtist);
 router.delete("/:id", authorize(["editor"]), deleteArtist);
 router.post("/add-artist", authorize(["editor"]), addArtist);
-router.put("/update-artist", authorize(["editor"]), updateArtist);
 export default router;
